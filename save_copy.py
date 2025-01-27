1733203e2f9b2a312d8d65f9a22da6d4ad5d2431
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 15:26:34 -0800

    f

--- save_copy.py
+++ save_copy.py
@@ -1,5 +1,5 @@
 # mdn = "amuvarma/3b-zuckreg-convo"
-# from transformers import AutoModelForCausalLM, AutoTokenizer, LlavaForConditionalGeneration
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlavaForConditionalGeneration
 # model = AutoModelForCausalLM.from_pretrained(mdn)
 # model = LlavaForConditionalGeneration.from_pretrained(mdn)
 # print(model)
