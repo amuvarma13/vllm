dee178727a932f83c73a4480257d0109da95180c
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 15:31:54 -0800

    f

--- save_copy.py
+++ save_copy.py
@@ -1,21 +1,19 @@
 # mdn = "amuvarma/3b-zuckreg-convo"
-from transformers import AutoModelForCausalLM, AutoTokenizer, LlavaForConditionalGeneration
-# model = AutoModelForCausalLM.from_pretrained(mdn)
+# from transformers import AutoModelForCausalLM, AutoTokenizer, LlavaForConditionalGeneration
+# # model = AutoModelForCausalLM.from_pretrained(mdn)
 # model = LlavaForConditionalGeneration.from_pretrained(mdn)
 # print(model)
-# model.save_pretrained("./zuckreg")
+# # model.save_pretrained("./zuckreg")
 
-from transformers import AutoModel
+from transformers import LlavaForCausalLM
 
-checkpoint_name = "./zuckreg"
-config_path = "./llavaconf.json"
+checkpoint_name = "amuvarma/3b-zuckreg-convo"
 
-model = LlavaForConditionalGeneration.from_pretrained(
-    pretrained_model_name_or_path=checkpoint_name,
-    config=config_path,
-    ignore_mismatched_sizes=True
-)
+model = LlavaForCausalLM.from_pretrained(checkpoint_name)
 
-print(model)
-
+# model = LlavaForCausalLM.from_pretrained(
+#     pretrained_model_name_or_path=checkpoint_name,
+#     config=config_path,
+#     ignore_mismatched_sizes=True
+# )
 
