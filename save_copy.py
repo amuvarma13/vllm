8e7e8a128f227ec9a7acd6467b645bee8c1cefcf
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 14:56:14 -0800

    f

--- save_copy.py
+++ save_copy.py
@@ -2,3 +2,4 @@ mdn = "amuvarma/3b-zuckreg-convo"
 from transformers import AutoModelForCausalLM, AutoTokenizer
 model = AutoModelForCausalLM.from_pretrained(mdn)
 print(model)
+model.save_pretrained("./3b-zuckreg-convo")
