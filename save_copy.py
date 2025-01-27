74e1e123b29a67aa54ce069a7483347ba6e4033f
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 15:22:20 -0800

    f

--- save_copy.py
+++ save_copy.py
@@ -5,16 +5,23 @@
 # print(model)
 # # model.save_pretrained("./zuckreg")
 
-from transformers import AutoModel
+# from transformers import AutoModel
 
-checkpoint_name = "amuvarma/3b-zuckreg-convo"
-config_path = "./llavaconf.json"
+# checkpoint_name = "amuvarma/3b-zuckreg-convo"
+# config_path = "./llavaconf.json"
 
-model = AutoModel.from_pretrained(
-    pretrained_model_name_or_path=checkpoint_name,
-    config=config_path,
-    ignore_mismatched_sizes=True
-)
+# model = AutoModel.from_pretrained(
+#     pretrained_model_name_or_path=checkpoint_name,
+#     config=config_path,
+#     ignore_mismatched_sizes=True
+# )
 
-print(model)
+# print(model)
 
+
+from huggingface_hub import hf_hub_download
+import torch
+
+local_path = hf_hub_download(repo_id="amuvarma/3b-zuckreg-convo", filename="pytorch_model.bin")
+state_dict = torch.load(local_path, map_location="cpu")
+print(state_dict.keys())
