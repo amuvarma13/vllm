b042f425a902a8876d2cb962242c0e35b75cfdeb
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 16:07:29 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -1,8 +1,12 @@
 mdn = "./zuckreg-llava"
 from vllm import ModelRegistry, LLM, SamplingParams
 import inspect
+from transformers import AutoTokenizer
+tkn = AutoTokenizer.from_pretrained(mdn)
 
 prompt = "Hello my name is"
+tokens = tkn(prompt, return_tensors="pt")
+print(tokens)
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model=mdn)
