8423c1c8f5ca61f4c9ec6e88e9baa5e2f18242cf
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 16:10:06 -0800

    f

--- save_tokeniser.py
+++ save_tokeniser.py
@@ -2,4 +2,7 @@ from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("amuvarma/3b-zuckreg-convo")
 
+
+print(len(tokenizer))
+
 tokenizer.save_pretrained("./zuckreg-llava")
