fd5ddd44f2f53d7a586b35b64aa78fe80919a75b
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 16:16:00 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -3,6 +3,7 @@ from vllm import ModelRegistry, LLM, SamplingParams
 import inspect
 from transformers import AutoTokenizer
 tkn = AutoTokenizer.from_pretrained(mdn)
+import torch
 
 prompt = "<image>"
 tokens = tkn(prompt, return_tensors="pt")
@@ -11,5 +12,11 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model=mdn)
 
-outputs = llm.generate(prompt, sampling_params)
+image_embeds = torch.randn(1, 3072)
 
+outputs = llm.generate({
+    "prompt": prompt,
+    "multi_modal_data": {"image": image_embeds},
+})
+
+print(outputs)
