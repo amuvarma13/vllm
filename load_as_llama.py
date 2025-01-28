9e8218b4dfffc19378965c776dea410923e798b4
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 10:47:20 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -4,7 +4,7 @@ import inspect
 from transformers import AutoTokenizer
 tkn = AutoTokenizer.from_pretrained(mdn)
 
-prompt = "<image>"
+prompt = "<|image|>"
 tokens = tkn(prompt, return_tensors="pt")
 print(tokens)
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
