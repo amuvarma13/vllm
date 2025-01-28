42091378a113b68ab7a6f9d8a304ea8a3e404613
Amartya Varma <amartya.varma@gmail.com>
2025-01-28 10:41:54 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -10,13 +10,8 @@ llm = LLM(model=mdn)
 
 image_embeds = torch.randn( 1, 3072)
 print(image_embeds.shape)
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+prompt = "Hello, my name is"
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-outputs = llm.generate(prompts, sampling_params)
+outputs = llm.generate(prompt, sampling_params)
 
 print(outputs)
