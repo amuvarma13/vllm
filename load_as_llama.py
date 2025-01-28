2905d98b62439224a571180494f3b9fd78ed57fe
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 16:37:56 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -1,23 +1,21 @@
-mdn = "./zuckreg-llava"
+mdn = "./zuckreg"
 from vllm import ModelRegistry, LLM, SamplingParams
 import inspect
 from transformers import AutoTokenizer
 tkn = AutoTokenizer.from_pretrained(mdn)
 import torch
 
-prompt = "<image>"
-tokens = tkn(prompt, return_tensors="pt")
-print(tokens)
+# tokens = tkn(prompt, return_tensors="pt")
+# print(tokens)
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model=mdn)
 
-image_embeds = torch.randn(1, 1, 3072)
+image_embeds = torch.randn( 1, 3072)
 print(image_embeds.shape)
 
 outputs = llm.generate({
-    "prompt": prompt,
-    "multi_modal_data": {"image": image_embeds},
+    "prompt_embeds": image_embeds,
 })
 
 print(outputs)
