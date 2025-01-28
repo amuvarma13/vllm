a9e97be7c4ad263bbabd57d118fc9f62e5d93ae9
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 16:01:28 -0800

    f

--- load_as_llama.py
+++ load_as_llama.py
@@ -2,21 +2,12 @@ mdn = "./zuckreg-llava"
 from vllm import ModelRegistry, LLM, SamplingParams
 import inspect
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
-    "The future of AI is",
-]
+prompt = "Hello my name is"
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model=mdn)
 
 
 
-outputs = llm.generate(prompts, sampling_params)
+outputs = llm.generate(prompt, sampling_params)
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
